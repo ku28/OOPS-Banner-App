e8f1fc2dbeb123067ada29741fe45b3cae9fd20a
ku28 <ku28sh07@gmail.com>
2025-12-29 10:02:20 +0530

    initial commit

---
+++ src/UC1.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display
+ *
+ * This class demonstrates that displays "OOPS" to the console
+ *
+ * @author Kushagra
+ * @version 1.0
+ */
+
+public class UC1 {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
